bacb36f30e19196c672dc36bec5a59ae9d392af1
Loreto <lgonzalezc@iesch.org>
2024-10-24 16:22:41 +0200

    Añadida la clase Constantes

---
+++ Constantes.java
@@ -0,0 +1,14 @@
+public class Constantes {
+    // Fuerza de la Gravedad g
+    public static final double G = 9.80665;
+
+    public static void main(String[] args) {
+        // Con qué fuerza atrae la tierra a un balón de fútbol
+        // Fórmula: F = m * g
+        double m = .450;
+        double f = m * G;
+        System.out.println("La tierra atrae a un balón de fútbol con una fuerza de " + f + " newtons.")
+
+    }
+    
+}
