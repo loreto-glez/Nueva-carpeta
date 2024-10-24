5cad824b5fa00136d2b196008d5e834d7586aff0
Loreto <lgonzalezc@iesch.org>
2024-10-24 16:13:58 +0200

    First commit

---
+++ Variables.java
@@ -0,0 +1,12 @@
+package ud1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + "b =" + b);
+    }
+}
